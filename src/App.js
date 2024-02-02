11a7c803e45fe853ee362dd2a7e76baf78c53716
arjun963 <arjun.bhattrai.7@gmail.com>
2024-02-03 02:22:56 +0530

    Add project files to Git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Square() {
+  return <div className="square">Image</div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Header</header>
+      <main className="App-main">
+        <aside className="App-sidebar">Aside</aside>
+        <article className="App-article">
+          <p class="text-right">Article</p>
+          <Square />
+          <Square />
+          <Square />
+        </article>
+      </main>
+      <footer className="App-footer">Footer</footer>
     </div>
   );
 }
